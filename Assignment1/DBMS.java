61e06e8fcbcc8e095a03c00f692880b7f6c12c34
YUKTA RAJKUMAR GURNANI <yk868576@dal.ca>
2024-02-25 18:08:11 -0400

    Upload New File

---
+++ Assignment1/DBMS.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+
+public class DBMS extends Query
+{
+    public static void main(String[] args) throws NoSuchAlgorithmException, IOException
+    {
+        /* Authentication method call for two-factor user authentication */
+        Authentication auth = new Authentication();
+        auth.Authentication();
+    }
+}
